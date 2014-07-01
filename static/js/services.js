391ff21f6b18b92655e1c427abac7001787af49b
Nima <nimaonsafari@gmail.com>
2014-07-01 18:47:52 +0200

    new colorpicker

--- static/js/services.js
+++ static/js/services.js
@@ -1,4 +1,4 @@
-var postiesApp = angular.module('posties', ['ngSanitize'], function ($interpolateProvider) {
+var postiesApp = angular.module('posties', ['ngSanitize', 'colorpicker.module'], function ($interpolateProvider) {
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
 });
