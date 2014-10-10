a228a3e67836685f091cb574d59738c7f8335b36
Anton Trollbäck <anton@trollback.se>
2014-10-10 13:50:18 +0200

    Better looking, and responsive, videos

--- static/js/util.js
+++ static/js/util.js
@@ -35,10 +35,11 @@ posties.util = (function() {
   // Get the YouTube video ID from a URL. Supports multiple YouTube URL types, such as embed and watch.
   // Will return false if the url parameter isn't a valid YouTube url
   var getYouTubeVideoID = function(url) {
+    var params = '?modestbranding=1&amp;showinfo=0&amp;showsearch=0&amp;rel=0';
     var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
     var match = url.match(regExp);
     if(match&&match[7].length==11) {
-      return match[7];
+      return match[7] + params;
     } else {
       return false;
     }
