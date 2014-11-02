79bc7040bf58b674adfb0683e930295a0aa0dbda
Anton Trollbäck <anton@trollback.se>
2014-11-02 14:33:44 +0100

    Fix image urls

--- static/js/filters.js
+++ static/js/filters.js
@@ -19,3 +19,13 @@ postiesApp.filter('safeYoutubeUrl', ['$sce', function($sce) {
     return $sce.trustAsResourceUrl('//www.youtube.com/embed/' + sourceId + '?modestbranding=1&amp;showinfo=0&amp;showsearch=0&amp;rel=0');
   };
 }]);
+
+postiesApp.filter('fixImageUrl', ['$sce', function($sce) {
+  return function(sourceId) {
+    if (sourceId.indexOf('filepicker') > -1) {
+      return sourceId + '/convert?cache=true&w=740&fit=max&rotate=exif&compress=true&quality=100';
+    } else {
+      return 'https://s3-eu-west-1.amazonaws.com/posties-images/' + sourceId;
+    }
+  };
+}]);
