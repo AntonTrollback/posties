44b72984e677c57612daf18cf8327a38b9bdb944
Anton Trollbäck <anton@trollback.se>
2015-01-16 15:00:43 +0100

    test

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
+var fs = require('fs');
+var Habitat = require('habitat');
 var express = require('express');
 var mustacheExpress = require('mustache-express');
-var app = express();
-var fs = require('fs');
 var compression = require('compression');
-var Habitat = require('habitat');
+var _ = require('lodash');
 var cssnext = require('cssnext');
 
 // Setup environment
 Habitat.load();
-var env = new Habitat('posties');
+var env = new Habitat();
 
-console.log(env.get('env'))
-
-app.set('port', (process.env.PORT || env.get('port') || 5000));
+// Setup express
+var app = express();
+var root = __dirname;
+app.set('port', process.env.PORT || 5000);
+app.use(express.static(root + '/public'));
 
 // Enable resource compression
 app.use(compression());
@@ -20,19 +22,16 @@ app.use(compression());
 // Configure mustache
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
-app.set('views', __dirname + '/ui/html');
-
-// Setup static file directory
-app.use(express.static(__dirname + '/public'));
+app.set('views', root + '/ui/html');
 
 /**
  * Parse CSS
  */
 
-var styleInput = fs.readFileSync(__dirname + '/ui/css/index.css', 'utf8');
-var styleOutput = cssnext(styleInput);
+var input = fs.readFileSync(root + '/ui/css/index.css', 'utf8');
+var output = cssnext(input);
 
-fs.writeFileSync(__dirname + '/public/index.css', styleOutput);
+fs.writeFileSync(root + '/public/index.css', output);
 
 /**
  * Requests
@@ -44,6 +43,7 @@ app.listen(app.get('port'), function() {
 
 app.get('/', function(req, res) {
   res.render('layout', {
+    production: env.get('port') === 'production',
     title: 'Posti.es',
     description: 'Posti.es description ...',
     content: 'hello world'
