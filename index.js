61a26e96236a4cf2f4eba9c4592656054391a639
Anton Trollbäck <anton@trollback.se>
2015-01-16 14:38:37 +0100

    test

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 var express = require('express');
+var mustacheExpress = require('mustache-express');
 var app = express();
+var fs = require('fs');
+var compression = require('compression');
+var Habitat = require('habitat');
+var cssnext = require('cssnext');
 
-app.set('port', (process.env.PORT || 5000));
+// Setup environment
+Habitat.load();
+var env = new Habitat('posties');
+
+console.log(env.get('env'))
+
+app.set('port', (process.env.PORT || env.get('port') || 5000));
+
+// Enable resource compression
+app.use(compression());
+
+// Configure mustache
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/ui/html');
+
+// Setup static file directory
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World! Bland annat Jakob!');
-});
+/**
+ * Parse CSS
+ */
+
+var styleInput = fs.readFileSync(__dirname + '/ui/css/index.css', 'utf8');
+var styleOutput = cssnext(styleInput);
+
+fs.writeFileSync(__dirname + '/public/index.css', styleOutput);
+
+/**
+ * Requests
+ */
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+  console.log('Running at localhost:' + app.get('port'));
+});
+
+app.get('/', function(req, res) {
+  res.render('layout', {
+    title: 'Posti.es',
+    description: 'Posti.es description ...',
+    content: 'hello world'
+  });
 });
